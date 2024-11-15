3ddbca920450db5618fd235a0acfe5e01e3d9e3c
camillazicari <camyzicari01@gmail.com>
2024-11-15 15:27:25 +0100

    es 23

--- assets/js/script.js
+++ assets/js/script.js
@@ -500,16 +500,16 @@ console.log(removeIndex(0));
   Scrivi una funzione per selezionare l'elemento dotato di id "container" all'interno della pagina.
 */
 
-const myContainer = function() {
-    const container = document.getElementById('container');
-}
+const myContainer = function () {
+  const container = document.getElementById("container");
+};
 /* ESERCIZIO 21
   Scrivi una funzione per selezionare ogni tag <td> all'interno della pagina.
 */
 
-const myCells = function() {
-    const cells = document.querySelectorAll('td');
-}
+const myCells = function () {
+  const cells = document.querySelectorAll("td");
+};
 
 /* ESERCIZIO 22
   Scrivi una funzione che, tramite un ciclo, stampa in console il testo contenuto in ogni tag <td> all'interno della pagina.
@@ -517,16 +517,26 @@ const myCells = function() {
 console.log("-----es 22-----");
 
 function print() {
-    const cells = document.querySelectorAll('td');
-    for (let i=0; i<cells.length; i++) {
-        console.log(cells[i].innerText);
-    }
+  const cells = document.querySelectorAll("td");
+  for (let i = 0; i < cells.length; i++) {
+    console.log(cells[i].innerText);
+  }
 }
 print();
 
 /* ESERCIZIO 23
   Scrivi una funzione per aggiungere un background di colore rosso a ogni link all'interno della pagina.
 */
+console.log("-----es 23-----");
+
+const links = document.querySelectorAll("a");
+
+function red() {
+  links.forEach((element) => {
+    element.style.backgroundColor = "red";
+  });
+}
+red();
 
 /* ESERCIZIO 24
   Scrivi una funzione per aggiungere un nuovo elemento alla lista non ordinata con id "myList".
